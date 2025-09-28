25b9ca3b8cf0cb6f01d3595693fe621aa0777089
satbek997 <abyzsatbek689@gmail.com>
2025-09-28 20:56:14 +0500

    Update Counters.java

--- src/metrics/Counters.java
+++ src/metrics/Counters.java
@@ -1,1 +1,28 @@
+package metrics;
 
+
+public class Counters {
+    public long comparisons = 0;
+    public long swaps = 0;
+    public long allocations = 0;
+    public int maxDepth = 0;
+    private int curDepth = 0;
+
+    public void enter() { 
+        curDepth++;
+        if (curDepth > maxDepth) maxDepth = curDepth;
+    }
+
+    public void exit() { 
+        curDepth--;
+    }
+
+    public void incComparisons() { comparisons++; }
+    public void incSwaps() { swaps++; }
+    public void incAllocations() { allocations++; }
+
+    public void reset() {
+        comparisons = swaps = allocations = 0;
+        curDepth = maxDepth = 0;
+    }
+}
