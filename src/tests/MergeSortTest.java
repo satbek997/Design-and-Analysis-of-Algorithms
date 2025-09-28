f2b7f12edc12706dd5090e0b14dc491dea9e3156
satbek997 <abyzsatbek689@gmail.com>
2025-09-28 21:04:13 +0500

    Update MergeSortTest.java

--- src/tests/MergeSortTest.java
+++ src/tests/MergeSortTest.java
@@ -1,1 +1,44 @@
+package tests;
 
+import metrics.Counters;
+import org.junit.jupiter.api.Test;
+import sort.MergeSort;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MergeSortTest {
+
+    @Test
+    void testCorrectnessRandomArray() {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+        int[] copy = Arrays.copyOf(arr, arr.length);
+
+        Counters c = new Counters();
+        MergeSort.sort(arr, c);
+
+        Arrays.sort(copy);
+        assertArrayEquals(copy, arr);
+    }
+
+    @Test
+    void testAlreadySorted() {
+        int[] arr = {1, 2, 3, 4, 5};
+        int[] copy = Arrays.copyOf(arr, arr.length);
+
+        Counters c = new Counters();
+        MergeSort.sort(arr, c);
+
+        Arrays.sort(copy);
+        assertArrayEquals(copy, arr);
+    }
+
+    @Test
+    void testSingleElement() {
+        int[] arr = {42};
+        Counters c = new Counters();
+        MergeSort.sort(arr, c);
+        assertArrayEquals(new int[]{42}, arr);
+    }
+}
